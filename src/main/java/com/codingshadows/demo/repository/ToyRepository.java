98bfb7eb672496ed32fd8783a24072a620b6f6d6
Rusu Dinu <dinu@codingshadows.com>
2023-05-12 17:39:18 +0300

    prepared for live demo

--- src/main/java/com/codingshadows/demo/repository/ToyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codingshadows.demo.repository;
-
-import com.codingshadows.demo.model.Toy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToyRepository extends JpaRepository<Toy, Long> {
-}
